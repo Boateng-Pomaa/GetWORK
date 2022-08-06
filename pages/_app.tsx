e92f631e159a725f7786458ccda8d126ac34ab93
Nana Dua <nanadua11@outlook.com>
2022-08-06 17:49:34 +0000

    Added fonts, ui changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { NextUIProvider } from '@nextui-org/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    // <NextUIProvider>
-    <Component {...pageProps} />
-    // </NextUIProvider>
-  );
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-// import type { NextPage } from 'next';
-// import Head from 'next/head';
-// import Image from 'next/image';
+import Head from 'next/head';
 import { useRef, useState } from 'react';
-import styles from '../styles/Home.module.css';
-// import styles from './index.module.css';
+import styles from '../styles/index.module.css';
 
 export default function Home() {
   const FORM_FIELDS = Object.freeze({
@@ -22,32 +19,49 @@ export default function Home() {
   const clickHandler = () => console.log('Clicked'); //! TYPES
 
   return (
-    <div className={styles.card}>
-      <header>
-        <button>Login</button>
-        <button>Signup</button>
+    <div className={styles.wrapper}>
+      <Head>
+        <title>Linda Pomaa</title>
+        {/* <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> */}
+      </Head>
+      <header className={styles.header}>
+        <h1>App name</h1>
+        <p>Create account</p>
       </header>
-      <form method="POST" onSubmit={onSubmitHandler}>
-        <div className={styles.input_group}>
-          <label htmlFor={FORM_FIELDS.USERNAME}>{FORM_FIELDS.USERNAME}</label>
-          <input type="text" id={FORM_FIELDS.USERNAME} />
+      <div className={styles.card}>
+        <form method="POST" onSubmit={onSubmitHandler}>
+          <header>
+            <button className={styles.toggle}>Login</button>
+            <button className={styles.toggle}>Signup</button>
+          </header>
+          <div className={styles.input_group}>
+            <label htmlFor={FORM_FIELDS.USERNAME}>{FORM_FIELDS.USERNAME}</label>
+            <input type="text" id={FORM_FIELDS.USERNAME} />
+          </div>
+          <div className={styles.input_group}>
+            <label htmlFor={FORM_FIELDS.EMAIL}>{FORM_FIELDS.EMAIL}</label>
+            <input type="text" id={FORM_FIELDS.EMAIL} />
+          </div>
+          <div className={styles.input_group}>
+            <label htmlFor={FORM_FIELDS.PASSWORD}>{FORM_FIELDS.PASSWORD}</label>
+            <input type="text" id={FORM_FIELDS.PASSWORD} />
+          </div>
+        </form>
+        <button type="submit" className={styles.button} onClick={clickHandler}>
+          Create account
+        </button>
+        <div>
+          <p>
+            Already have an account?
+            <button
+              type="submit"
+              className={styles.button}
+              onClick={clickHandler}
+            >
+              Login
+            </button>
+          </p>
         </div>
-        <div className={styles.input_group}>
-          <label htmlFor={FORM_FIELDS.EMAIL}>{FORM_FIELDS.EMAIL}</label>
-          <input type="text" id={FORM_FIELDS.EMAIL} />
-        </div>
-        <div className={styles.input_group}>
-          <label htmlFor={FORM_FIELDS.PASSWORD}>{FORM_FIELDS.PASSWORD}</label>
-          <input type="text" id={FORM_FIELDS.PASSWORD} />
-        </div>
-      </form>
-      <button type="submit" className="button" onClick={clickHandler}>
-        Create account
-      </button>
-      <div>
-        <p>
-          Already have an account? <a href="#">Login</a>
-        </p>
       </div>
     </div>
   );
